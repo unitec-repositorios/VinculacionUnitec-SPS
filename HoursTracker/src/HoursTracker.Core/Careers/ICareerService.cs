322118b96216865ae5425b1e67f00fb8dc546509
vincentsbarrios <vincentsbarrios@users.noreply.github.com>
2020-05-14 15:02:47 -0600

    Tabla de mantenimiento
    Web:
    The view create, index & edit were created.
    Table created
    Without relationship

---
+++ HoursTracker/src/HoursTracker.Core/Careers/ICareerService.cs
@@ -0,0 +1,20 @@
+﻿using HoursTracker.Domain.Aggregates.Careers;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoursTracker.Core.Careers
+{
+    public interface ICareerService
+    {
+        Task<Career> FindById(int id);
+
+        Task Create(Career career);
+
+        Task<IEnumerable<Career>> All();
+
+        Task Remove(int id);
+
+        Task Update(int id, Career career);
+    }
+}
