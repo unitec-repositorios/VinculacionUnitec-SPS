a99855a79be42d0c4d1c04c3f7ead1896add55f2
sarah <38963891+sarahcastelar@users.noreply.github.com>
2020-08-03 22:55:51 -0600

    Add files via upload

---
+++ exportar-importar/exportarBaseDatosVinculacion/MainForm.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace exportarBaseDatosVinculacion
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.exportButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.importButton = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // exportButton
+            // 
+            this.exportButton.BackColor = System.Drawing.Color.LimeGreen;
+            this.exportButton.ForeColor = System.Drawing.Color.Black;
+            this.exportButton.Location = new System.Drawing.Point(46, 147);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Size = new System.Drawing.Size(118, 40);
+            this.exportButton.TabIndex = 0;
+            this.exportButton.Text = "Exportar base datos";
+            this.exportButton.UseVisualStyleBackColor = false;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ultima exportacion: ";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.importButton);
+            this.panel1.Controls.Add(this.progressBar);
+            this.panel1.Controls.Add(this.closeButton);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.exportButton);
+            this.panel1.Location = new System.Drawing.Point(134, 37);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(357, 247);
+            this.panel1.TabIndex = 2;
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(46, 64);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(249, 37);
+            this.progressBar.TabIndex = 8;
+            this.progressBar.Visible = false;
+            // 
+            // closeButton
+            // 
+            this.closeButton.BackColor = System.Drawing.Color.Red;
+            this.closeButton.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.closeButton.Location = new System.Drawing.Point(207, 202);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(118, 32);
+            this.closeButton.TabIndex = 7;
+            this.closeButton.Text = "Salir";
+            this.closeButton.UseMnemonic = false;
+            this.closeButton.UseVisualStyleBackColor = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // importButton
+            // 
+            this.importButton.BackColor = System.Drawing.Color.LimeGreen;
+            this.importButton.ForeColor = System.Drawing.Color.Black;
+            this.importButton.Location = new System.Drawing.Point(177, 147);
+            this.importButton.Name = "importButton";
+            this.importButton.Size = new System.Drawing.Size(118, 40);
+            this.importButton.TabIndex = 9;
+            this.importButton.Text = "Importar base datos";
+            this.importButton.UseVisualStyleBackColor = false;
+            this.importButton.Click += new System.EventHandler(this.importButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AcceptButton = this.closeButton;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(615, 342);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Exportar Base de Datos ";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.MainForm_FormClosed);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button exportButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button importButton;
+    }
+}
---
+++ exportar-importar/exportarBaseDatosVinculacion/MainForm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace exportarBaseDatosVinculacion
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            progressBar.Visible = true;
+            SqlConnection cnn;
+            string connectionstring = null;
+            string sql = null;
+
+            _Excel.Application xlApp;
+            _Excel.Workbook xlWorkBook;
+            _Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlApp = new Microsoft.Office.Interop.Excel.Application();
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            xlWorkSheet = (_Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            connectionstring = "Server=(localdb)\\MSSQLLocalDB;Database=HoursTracker;Trusted_Connection=True;";
+            cnn = new SqlConnection(connectionstring);
+            cnn.Open();
+
+            sql = "SELECT * FROM usuarios";
+
+            SqlDataAdapter dscmd = new SqlDataAdapter(sql, cnn);
+            DataSet ds = new DataSet();
+            dscmd.Fill(ds);
+
+            System.Data.DataTable miTabla = ds.Tables[0];
+
+            for (int i = 1; i < miTabla.Columns.Count + 1; i++)
+            {
+                xlWorkSheet.Cells[1, i] = miTabla.Columns[i - 1].ColumnName;
+            }
+
+            for (int j = 0; j < miTabla.Rows.Count; j++)
+            {
+                for (int k = 0; k < miTabla.Columns.Count; k++)
+                {
+                    xlWorkSheet.Cells[j + 2, k + 1] = miTabla.Rows[j].ItemArray[k].ToString();
+                }
+            }
+
+            xlApp.Visible = true;
+
+            sql = "SELECT * FROM campuses";
+
+            dscmd = null;
+            ds = null;
+            miTabla = null;
+
+            _Excel.Sheets worksheets = xlWorkBook.Worksheets;
+            var xlNewSheet = (_Excel.Worksheet)worksheets.Add(worksheets[1], Type.Missing, Type.Missing, Type.Missing);
+            xlNewSheet.Name = "newsheet";
+
+            xlNewSheet = (_Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            dscmd = new SqlDataAdapter(sql, cnn);
+            ds = new DataSet();
+            dscmd.Fill(ds);
+
+            miTabla = ds.Tables[0];
+
+            for (int i = 1; i < miTabla.Columns.Count + 1; i++)
+            {
+                xlNewSheet.Cells[1, i] = miTabla.Columns[i - 1].ColumnName;
+            }
+
+            for (int j = 0; j < miTabla.Rows.Count; j++)
+            {
+                for (int k = 0; k < miTabla.Columns.Count; k++)
+                {
+                    xlNewSheet.Cells[j + 2, k + 1] = miTabla.Rows[j].ItemArray[k].ToString();
+                }
+            }
+
+            xlApp.Visible = true;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result;
+            string caption = "Salir";
+            MessageBoxButtons buttons = MessageBoxButtons.YesNo;
+            result = MessageBox.Show("Esta seguro que desea salir?", caption, buttons);
+            if (result == System.Windows.Forms.DialogResult.Yes)
+            {
+                Application.Exit();
+            }
+        }
+
+        private void MainForm_FormClosed(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void importButton_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
